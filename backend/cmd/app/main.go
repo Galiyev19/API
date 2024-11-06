0040f6abad79d4225f3f9340287eebb072975dbb
galiyev19 <galiyevalisher7@gmail.com>
2024-11-06 14:02:27 +0500

    started new API

--- backend/cmd/app/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"API/internal/app"
-)
-
-func main() {
-	app.Run()
-}
--- backend/migration/migration.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS users(
-    user_id TEXT NOT NULL UNIQUE,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL
-);
